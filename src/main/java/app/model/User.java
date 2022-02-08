1c3bae734b58c0600de11679917a5ecd8ec009c5
Finnian Rogers <finnianrogers@gmail.com>
2022-02-08 23:10:18 +0000

    began start of users model. Unsure on how the relationship between User and Appointments table works (1-1 or 1-many). Can complete it once we know

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,4 +1,39 @@
-package app.model;
 
+package app.model;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotBlank
+    private String dob;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String surname;
+    @NotBlank
+    private String ppsn;
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String phone;
+    @NotBlank
+    private String email;
+
+    // Unsure exactly how to represent appointment id as a foreign key here
+    // was following this guide https://www.baeldung.com/jpa-mapping-single-entity-to-multiple-tables
+    // but unsure on whether we have it as one to many or one to one. Will wait to continue the model until
+    // we know for sure
+//
+//    @OneToOne
+//    @PrimaryKeyJoinColumn(name = "aptId")
+//    private long nextAppointmentId;
+
 }
