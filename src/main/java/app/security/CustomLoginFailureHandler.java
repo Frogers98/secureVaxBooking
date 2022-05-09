7909a0f71a6dbbb273d2a100b807052ba17a410e
Finnian Rogers <finnianrogers@gmail.com>
2022-05-09 19:28:26 +0200

    added loginFailureHandler and necessary methods to UserRepository and UserService for this to work correctly

---
+++ src/main/java/app/security/CustomLoginFailureHandler.java
@@ -0,0 +1,51 @@
+package app.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import app.model.User;
+import app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+// Class added while following this guide: https://www.codejava.net/frameworks/spring-boot/spring-security-limit-login-attempts-example
+@Component
+public class CustomLoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String email = request.getParameter("email");
+        User user = userService.getByEmail(email);
+
+        if (user != null) {
+            if (user.isAccountNonLocked()) {
+                if (user.getFailedAttempt() < UserService.MAX_FAILED_ATTEMPTS - 1) {
+                    userService.increaseFailedAttempts(user);
+                } else {
+                    userService.lock(user);
+                    exception = new LockedException("Your account has been locked due to 3 failed attempts."
+                            + " It will be unlocked after 24 hours.");
+                }
+            } else if (!user.isAccountNonLocked()) {
+                if (userService.unlockWhenTimeExpired(user)) {
+                    exception = new LockedException("Your account has been unlocked. Please try to login again.");
+                }
+            }
+
+        }
+
+        super.setDefaultFailureUrl("/login?error");
+        super.onAuthenticationFailure(request, response, exception);
+    }
+
+}
